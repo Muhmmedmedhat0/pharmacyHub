f129cb56843e0220e49feb869ab205341048155a
Muhmmad Medhat <muhmmedmedhat0@gmail.com>
2024-05-02 19:33:31 +0300

    change token 2 userid

--- src/Routers/ProtectedRoute.jsx
+++ src/Routers/ProtectedRoute.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
-const getCookie = (name) => {
+export const getCookie = (name) => {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(";").shift();
 };
 
 const ProtectedRoute = () => {
-  const token = getCookie("token");
-console.log(token)
-  return token ? <Outlet /> : <Navigate to="/" />;
+  const USERID = getCookie("id");
+console.log(USERID)
+  return USERID ? <Outlet /> : <Navigate to="/" />;
 };
 
 
