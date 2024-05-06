8ff2ee5b2d336cafced987ad3e475c044d001ab1
Muhmmad Medhat <muhmmedmedhat0@gmail.com>
2024-05-06 17:40:43 +0300

    add update user

--- src/Redux/Slice/user.jsx
+++ src/Redux/Slice/user.jsx
@@ -1,43 +1,74 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getCookie } from '../../Routers/ProtectedRoute';
 
 const URL = 'http://e-pharmacy.runasp.net/api/account';
 
-export const logIn = createAsyncThunk('user/logIn', async (values, thunkAPI) => {
-  const { rejectWithValue } = thunkAPI;
+export const logIn = createAsyncThunk(
+  'user/logIn',
+  async (values, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
 
-  try {
-    const response = await fetch(`${URL}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-      },
-      body: JSON.stringify(values),
-    });
-    const data = await response.json();
-    document.cookie = `token=${data.token}`;
-    return data;
-  } catch (error) {
-    rejectWithValue(error);
-  }
-});
+    try {
+      const response = await fetch(`${URL}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify(values),
+      });
+      const data = await response.json();
+      document.cookie = `token=${data.token}`;
+      document.cookie = `id=${data.id}`;
+      return data;
+    } catch (error) {
+      rejectWithValue(error);
+    }
+  },
+);
 
-export const register = createAsyncThunk('user/register', async (values, thunkAPI) => {
-  const { rejectWithValue } = thunkAPI;
+export const register = createAsyncThunk(
+  'user/register',
+  async (values, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
 
-  try {
-    const response = await fetch(`${URL}/register`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-      },
-      body: JSON.stringify(values),
-    });
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    rejectWithValue(error);
-  }
-});
+    try {
+      const response = await fetch(`${URL}/register`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify(values),
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      rejectWithValue(error);
+    }
+  },
+);
+
+// update user data
+export const updateUser = createAsyncThunk(
+  'user/updateUser',
+  async (values, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    const token = getCookie('token'); // Retrieve the token
+    try {
+      const response = await fetch(`${URL}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+        },
+        body: JSON.stringify(values),
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      rejectWithValue(error);
+    }
+  },
+);
 
 const userSlice = createSlice({
   name: 'user',
@@ -48,6 +79,7 @@ const userSlice = createSlice({
   },
   reducers: {}, // Add any other reducers here if needed
   extraReducers: (builder) => {
+    // login
     builder
       .addCase(logIn.pending, (state) => {
         state.loading = true;
@@ -61,6 +93,7 @@ const userSlice = createSlice({
         state.loading = false;
         state.error = action.payload;
       })
+      // register
       .addCase(register.pending, (state) => {
         state.loading = true;
         state.error = null;
@@ -70,6 +103,20 @@ const userSlice = createSlice({
         state.userInfo = action.payload;
       })
       .addCase(register.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+      // update user data
+      builder
+      .addCase(updateUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.userInfo = action.payload;
+      })
+      .addCase(updateUser.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
       });
